506c77a0b9fd21649c7da831234ddb6a10164641
Ali Montajebi <montajebi.ali@gmail.com>
2021-09-30 15:31:45 +0330

    ADD: categories filed

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-    },
-    price: {
-        type: Number,
-        min: 0,
-        required: true
-    },
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  price: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  categories: {
+    type: Array,
+    default: [],
+  },
 });
 
 const Product = mongoose.model('Product', productSchema);
